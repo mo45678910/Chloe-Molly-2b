c706e6eb7fd10bb46c68832ea32031e3e6710182
mo45678910 <meolsen@wpi.edu>
2026-03-30 15:27:00 -0400

    Hobby One done

---
+++ src/components/hobby1.tsx
@@ -0,0 +1,22 @@
+type HobbyOneProps = {
+    hobby: string;
+    name: string;
+}
+
+export function HobbyOneWithProps(props: HobbyOneProps) {
+    return (
+        <div>
+            <p>My name is {props.name} and my hobby is {props.hobby}.
+                I went to Prague
+                for IQP this past fall and loved planning trips for my cohort every weekend. In 2025, I visited
+                11 new countries! I recently planned a trip to Iceland for my family and we will be going in May.</p>
+            <img src="../public/prague.jpg" width="481" alt="Prague" />
+        </div>
+    )
+}
+
+export function HobbyOneDemo() {
+    return (
+        <HobbyOneWithProps hobby="Travelling" name="Molly" />
+    )
+}
